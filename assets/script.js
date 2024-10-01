732792d3c81da28aa0692ba43c0242074c8f48d4
Mizami <riraku14@gmail.com>
2024-10-01 12:42:53 +0800

    update all
    added new sound and animation

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 const radenCharacter = document.querySelector(".raden-image");
-const audioFirst = "./assets/audio/maitake-1.mp3";
-const audioSecond = "./assets/audio/guru-1.mp3";
+const audioMaitake1 = "./assets/audio/maitake-1.mp3";
+const audioGuru1 = "./assets/audio/guru-1.mp3";
+const audioGuruE = "./assets/audio/guru-end.mp3";
 
 const spiralImage = document.querySelector(".spiral-img");
 
@@ -11,11 +12,12 @@ let timeouts = [];
 
 // Preload images
 const images = [
+    "assets/images/raden.png",
     "assets/images/raden-l-1.png",
     "assets/images/raden-l-2.png",
     "assets/images/raden-r-1.png",
     "assets/images/raden-r-2.png",
-    "assets/images/raden.png"
+    "assets/images/raden-f.png"
 ];
 
 images.forEach(src => {
@@ -32,19 +34,33 @@ radenCharacter.addEventListener("keydown", function(event){
 });
 
 function radenAction() {
-    clearAllTimeouts()
+    clearAllTimeouts();
+    radenCharacter.classList.remove("raden-spin-animation");
     ctr++;
     let randomNumber = Math.floor(Math.random() * 10)
 
-    if (ctr === 1) {
-        playAudio(audioFirst);
+    if (ctr % 2 != 0) {
+        playAudio(audioMaitake1);
         radenAnimation("まいたけ<br>");
+        console.log(`_____${ctr}______`)
     }
-    else if (ctr === 2) {
-        playAudio(audioSecond);
-        radenAnimation("ぐるぐる<br>");
-        ctr = 0;
+    else if (ctr % 2 === 0) {
+        if (ctr % 8 === 0) {
+            radenCharacter.src = "assets/images/raden-f.png"
+            radenCharacter.classList.add("raden-spin-animation");
+            playAudio(audioGuruE);
+            timeouts.push(setTimeout(function(){
+                radenCharacter.classList.remove("raden-spin-animation");
+                console.log("Spin stop");
+            }, 720));
+        }
+        else {
+            playAudio(audioGuru1);
+            radenAnimation("ぐるぐる<br>");
+            console.log(`_____${ctr}______`);
     }
+        }   
+
 }
 
 function playAudio(src) {
